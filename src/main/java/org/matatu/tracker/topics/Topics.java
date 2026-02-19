4c8a02c938af7b8c84fbfa14405acf099adf6aca
Pinchez25 <thuamwangi001@gmail.com>
2026-02-19 13:15:24 +0300

    feat(topics): add Topics class with Kafka topic name constants

---
+++ src/main/java/org/matatu/tracker/topics/Topics.java
@@ -0,0 +1,10 @@
+package org.matatu.tracker.topics;
+
+public final class Topics {
+
+    private Topics() {
+    }
+
+    public static final String MATATU_LOCATION = "matatu.location";
+    public static final String MATATU_FARES = "matatu.fares";
+}
