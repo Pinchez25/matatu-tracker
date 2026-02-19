e116c8fb2b9dde761636f02c3120fb5b4b8fb5ef
Pinchez25 <thuamwangi001@gmail.com>
2026-02-19 12:48:10 +0300

    feat: add spring boot application main class

---
+++ src/main/java/org/matatu/tracker/MatatuTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.matatu.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatatuTrackerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MatatuTrackerApplication.class, args);
+    }
+
+}
