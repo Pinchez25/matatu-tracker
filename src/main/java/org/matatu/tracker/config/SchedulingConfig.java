7e8b8994fd0a17517289ab88fb5104d1428460d7
Pinchez25 <thuamwangi001@gmail.com>
2026-02-19 13:08:43 +0300

    feat(config): add scheduling configuration

---
+++ src/main/java/org/matatu/tracker/config/SchedulingConfig.java
@@ -0,0 +1,16 @@
+package org.matatu.tracker.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(
+        name = "app.scheduling.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
+public class SchedulingConfig {
+}
+
