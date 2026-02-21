1af824dff74fb14cf21f3a749f46797e3be2b038
Pinchez25 <thuamwangi001@gmail.com>
2026-02-21 14:22:54 +0300

    feat(topics): add constants for speed alerts, enriched location, and failed fares topics

--- src/main/java/org/matatu/tracker/topics/Topics.java
+++ src/main/java/org/matatu/tracker/topics/Topics.java
@@ -7,4 +7,8 @@ public final class Topics {
 
     public static final String MATATU_LOCATION = "matatu.location";
     public static final String MATATU_FARES = "matatu.fares";
+
+    public static final String MATATU_SPEED_ALERTS  = "matatu.speed.alerts";
+    public static final String MATATU_LOCATION_ENRICHED = "matatu.location.enriched";
+    public static final String MATATU_FARES_FAILED = "matatu.fares.failed";
 }
