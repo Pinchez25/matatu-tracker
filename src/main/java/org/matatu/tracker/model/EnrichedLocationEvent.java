369f96bd5972f6a54e8c68db2322182e022c2460
Pinchez25 <thuamwangi001@gmail.com>
2026-02-21 14:23:15 +0300

    feat(model): add EnrichedLocationEvent record to represent enriched location data

---
+++ src/main/java/org/matatu/tracker/model/EnrichedLocationEvent.java
@@ -0,0 +1,14 @@
+package org.matatu.tracker.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.Instant;
+
+public record EnrichedLocationEvent(String matatuId, String routeId, String routeName, String saccoName, String saccoId,
+                                    String terminus, double latitude, double longitude, double speedKmh,
+                                    int passengersOnboard, boolean isSpeeding,
+
+                                    @JsonFormat(shape = JsonFormat.Shape.STRING) Instant originalTimestamp,
+
+                                    @JsonFormat(shape = JsonFormat.Shape.STRING) Instant enrichedAt) {
+}
